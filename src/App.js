458b67bb003a30d8f24a62ad8ca645ddb0917c48
JamesLyddon <jameslyddon@gmail.com>
2023-03-09 13:35:10 +0000

    data grid implemented

--- src/App.js
+++ src/App.js
@@ -1,325 +1,141 @@
 import './App.css'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
-import {
-  Checkbox,
-  FormControlLabel,
-  TextField,
-  Typography,
-} from '@mui/material'
-
-import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded'
-import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded'
+import { Container, Grid, TextField } from '@mui/material'
 
 import CityCard from './components/CityCard'
+import WeatherDataGrid from './components/WeatherDataGrid'
 
 function App() {
   const apiKey = 'd34d7bc68efa4646b33143938230303'
+  const [searchTerm, setSearchTerm] = useState('')
+  const [forecastWeather, setForecastWeather] = useState()
+  const [favouriteLocations, setFavouriteLocations] = useState([
+    'paris',
+    'london',
+  ])
+  const [favouriteWeather, setFavouriteWeather] = useState([])
+  const [useCelcius, setUseCelcius] = useState(true)
 
-  const [searchTerm, setSearchTerm] = useState('London')
-  const [favourite, setFavourite] = useState(false)
-  const [favLocationNames, setFavLocationNames] = useState([])
-  const [favLocationData, setFavLocationData] = useState([])
-  const [currentWeather, setCurrentWeather] = useState({
-    location: {
-      name: 'London',
-      region: 'City of London, Greater London',
-      country: 'United Kingdom',
-      lat: 51.52,
-      lon: -0.11,
-      tz_id: 'Europe/London',
-      localtime_epoch: 1678196002,
-      localtime: '2023-03-07 13:33',
-    },
-    current: {
-      last_updated_epoch: 1678195800,
-      last_updated: '2023-03-07 13:30',
-      temp_c: 3.0,
-      temp_f: 37.4,
-      is_day: 1,
-      condition: {
-        text: 'Light sleet',
-        icon: '//cdn.weatherapi.com/weather/64x64/day/317.png',
-        code: 1204,
-      },
-      wind_mph: 6.9,
-      wind_kph: 11.2,
-      wind_degree: 330,
-      wind_dir: 'NNW',
-      pressure_mb: 1000.0,
-      pressure_in: 29.53,
-      precip_mm: 0.2,
-      precip_in: 0.01,
-      humidity: 81,
-      cloud: 100,
-      feelslike_c: 0.6,
-      feelslike_f: 33.1,
-      vis_km: 10.0,
-      vis_miles: 6.0,
-      uv: 1.0,
-      gust_mph: 6.3,
-      gust_kph: 10.1,
-    },
-  })
-
-  // autocomplete weather search by location
   useEffect(() => {
-    getWeather()
-    console.log(favLocationData)
+    getForecastWeather()
   }, [searchTerm])
 
-  // update favourite location weather data
   useEffect(() => {
-    favLocationNames.forEach((name) => {
-      const getFavWeather = async () => {
-        try {
-          // early return to prevent serch queries under 3 letters
-          if (searchTerm.length < 3) return
-          const res = await axios.get(
-            `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${name}`,
-            {
-              withCredentials: false,
-            }
-          )
-          setFavLocationData((prev) => [...prev, res.data])
-        } catch (err) {
-          console.log(err)
-        }
-      }
-      getFavWeather()
-    })
-  }, [favLocationNames])
+    getFavWeather()
+  }, [])
 
-  const getWeather = async () => {
+  useEffect(() => {
+    console.log(forecastWeather)
+  }, [forecastWeather])
+
+  const getForecastWeather = async () => {
     try {
-      // early return to prevent serch queries under 3 letters
       if (searchTerm.length < 3) return
       const res = await axios.get(
-        `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${searchTerm}`,
+        `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${searchTerm}`,
         {
           withCredentials: false,
         }
       )
-      console.log(res.data)
-      setCurrentWeather(res.data)
-      if (favLocationNames.includes(res.data.location.name)) {
-        setFavourite(true)
-      } else {
-        setFavourite(false)
-      }
+      setForecastWeather(res.data)
     } catch (err) {
       console.log(err)
     }
   }
 
-  const onFavourite = (e) => {
-    setFavourite(e.target.checked)
-    if (
-      !favourite &&
-      !favLocationNames.includes(currentWeather.location.name)
-    ) {
-      // add location to favourite list
-      setFavLocationNames((prev) => [...prev, currentWeather.location.name])
-    } else if (favourite) {
-      //remove location from favourite list
-      setFavLocationNames((prev) =>
-        prev.filter((name) => name !== currentWeather.location.name)
+  const getFavWeather = async () => {
+    try {
+      if (favouriteLocations < 1) return
+      let urls = favouriteLocations.map(
+        (city) =>
+          `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}`
       )
+
+      const requests = urls.map((url) =>
+        axios.get(url, {
+          withCredentials: false,
+        })
+      )
+      axios.all(requests).then((responses) => {
+        setFavouriteWeather(responses)
+      })
+    } catch (err) {
+      console.log(err)
     }
   }
 
+  const weatherData = favouriteWeather.map((entry, index) => {
+    let currentHour = parseInt(entry.data.location.localtime.slice(-5, -3))
+
+    return {
+      id: index + 1,
+      city: entry.data.location.name,
+      country: entry.data.location.country,
+      icon: entry.data.current.condition.icon,
+      current_temp: useCelcius
+        ? entry.data.current.temp_c.toFixed()
+        : entry.data.current.temp_f.toFixed(),
+      current_datetime: entry.data.location.localtime.slice(-5),
+      plus_one_temp_c: useCelcius
+        ? entry.data.forecast.forecastday[0].hour[
+            currentHour + 1
+          ].temp_c.toFixed()
+        : entry.data.forecast.forecastday[0].hour[
+            currentHour + 1
+          ].temp_f.toFixed(),
+      plus_two_temp_c: useCelcius
+        ? entry.data.forecast.forecastday[0].hour[
+            currentHour + 2
+          ].temp_c.toFixed()
+        : entry.data.forecast.forecastday[0].hour[
+            currentHour + 2
+          ].temp_f.toFixed(),
+      plus_three_temp_c: useCelcius
+        ? entry.data.forecast.forecastday[0].hour[
+            currentHour + 3
+          ].temp_c.toFixed()
+        : entry.data.forecast.forecastday[0].hour[
+            currentHour + 3
+          ].temp_f.toFixed(),
+      plus_four_temp_c: useCelcius
+        ? entry.data.forecast.forecastday[0].hour[
+            currentHour + 4
+          ].temp_c.toFixed()
+        : entry.data.forecast.forecastday[0].hour[
+            currentHour + 4
+          ].temp_f.toFixed(),
+    }
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={favourite}
-              onChange={(e) => onFavourite(e)}
-              icon={<FavoriteBorderRoundedIcon />}
-              checkedIcon={<FavoriteRoundedIcon />}
-            />
-          }
-        />
-        <Typography variant="h3">
-          {currentWeather.current.temp_c}&deg;C
-        </Typography>
-        <Typography variant="h3">{currentWeather.location.name}</Typography>
-        <Typography variant="h6">{currentWeather.location.country}</Typography>
+      <Container sx={{ justifyContent: 'center' }}>
+        <Grid container direction="column">
+          <Grid item sx={{ m: 'auto' }}>
+            {forecastWeather && (
+              <CityCard
+                currentWeather={forecastWeather}
+                setFavouriteLocations={setFavouriteLocations}
+                setFavouriteWeather={setFavouriteWeather}
+              />
+            )}
+          </Grid>
+        </Grid>
         <TextField
+          inputProps={{ style: { textAlign: 'center', margin: '20px' } }}
           variant="standard"
           placeholder="city"
           onChange={(e) => setSearchTerm(e.target.value.trim())}
         />
-        <Typography variant="p">
-          {favLocationData.map((item) => (
-            <CityCard key={item.location.lon} currentWeather={item} />
-          ))}
-        </Typography>
-      </header>
+        <WeatherDataGrid
+          weatherData={weatherData}
+          useCelcius={useCelcius}
+          setUseCelcius={setUseCelcius}
+        />
+      </Container>
     </div>
   )
 }
 
 export default App
-
-// import './App.css'
-// import axios from 'axios'
-// import { useState, useEffect } from 'react'
-// import {
-//   Checkbox,
-//   FormControlLabel,
-//   TextField,
-//   Typography,
-// } from '@mui/material'
-
-// import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded'
-// import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded'
-
-// function App() {
-//   const apiKey = 'd34d7bc68efa4646b33143938230303'
-
-//   const [searchTerm, setSearchTerm] = useState('London')
-//   const [favourite, setFavourite] = useState(false)
-//   const [favLocationNames, setFavLocationNames] = useState([])
-//   const [favLocationData, setFavLocationData] = useState([])
-//   const [currentWeather, setCurrentWeather] = useState({
-//     location: {
-//       name: 'London',
-//       region: 'City of London, Greater London',
-//       country: 'United Kingdom',
-//       lat: 51.52,
-//       lon: -0.11,
-//       tz_id: 'Europe/London',
-//       localtime_epoch: 1678196002,
-//       localtime: '2023-03-07 13:33',
-//     },
-//     current: {
-//       last_updated_epoch: 1678195800,
-//       last_updated: '2023-03-07 13:30',
-//       temp_c: 3.0,
-//       temp_f: 37.4,
-//       is_day: 1,
-//       condition: {
-//         text: 'Light sleet',
-//         icon: '//cdn.weatherapi.com/weather/64x64/day/317.png',
-//         code: 1204,
-//       },
-//       wind_mph: 6.9,
-//       wind_kph: 11.2,
-//       wind_degree: 330,
-//       wind_dir: 'NNW',
-//       pressure_mb: 1000.0,
-//       pressure_in: 29.53,
-//       precip_mm: 0.2,
-//       precip_in: 0.01,
-//       humidity: 81,
-//       cloud: 100,
-//       feelslike_c: 0.6,
-//       feelslike_f: 33.1,
-//       vis_km: 10.0,
-//       vis_miles: 6.0,
-//       uv: 1.0,
-//       gust_mph: 6.3,
-//       gust_kph: 10.1,
-//     },
-//   })
-
-//   // autocomplete weather search by location
-//   useEffect(() => {
-//     getWeather()
-//     console.log(favLocationData)
-//   }, [searchTerm])
-
-//   // update favourite location weather data
-//   useEffect(() => {
-//     favLocationNames.forEach((name) => {
-//       const getFavWeather = async () => {
-//         try {
-//           // early return to prevent serch queries under 3 letters
-//           if (searchTerm.length < 3) return
-//           const res = await axios.get(
-//             `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${name}`,
-//             {
-//               withCredentials: false,
-//             }
-//           )
-//           setFavLocationData((prev) => [...prev, res.data])
-//         } catch (err) {
-//           console.log(err)
-//         }
-//       }
-//       getFavWeather()
-//     })
-//   }, [favLocationNames])
-
-//   const getWeather = async () => {
-//     try {
-//       // early return to prevent serch queries under 3 letters
-//       if (searchTerm.length < 3) return
-//       const res = await axios.get(
-//         `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${searchTerm}`,
-//         {
-//           withCredentials: false,
-//         }
-//       )
-//       console.log(res.data)
-//       setCurrentWeather(res.data)
-//       if (favLocationNames.includes(res.data.location.name)) {
-//         setFavourite(true)
-//       } else {
-//         setFavourite(false)
-//       }
-//     } catch (err) {
-//       console.log(err)
-//     }
-//   }
-
-//   const onFavourite = (e) => {
-//     setFavourite(e.target.checked)
-//     if (
-//       !favourite &&
-//       !favLocationNames.includes(currentWeather.location.name)
-//     ) {
-//       // add location to favourite list
-//       setFavLocationNames((prev) => [...prev, currentWeather.location.name])
-//     } else if (favourite) {
-//       //remove location from favourite list
-//       setFavLocationNames((prev) =>
-//         prev.filter((name) => name !== currentWeather.location.name)
-//       )
-//     }
-//   }
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <FormControlLabel
-//           control={
-//             <Checkbox
-//               checked={favourite}
-//               onChange={(e) => onFavourite(e)}
-//               icon={<FavoriteBorderRoundedIcon />}
-//               checkedIcon={<FavoriteRoundedIcon />}
-//             />
-//           }
-//         />
-//         <Typography variant="h3">
-//           {currentWeather.current.temp_c}&deg;C
-//         </Typography>
-//         <Typography variant="h3">{currentWeather.location.name}</Typography>
-//         <Typography variant="h6">{currentWeather.location.country}</Typography>
-//         <TextField
-//           variant="standard"
-//           placeholder="city"
-//           onChange={(e) => setSearchTerm(e.target.value.trim())}
-//         />
-//         <Typography variant="p">
-//           {favLocationData.map((item) => item.location.region)}
-//         </Typography>
-//       </header>
-//     </div>
-//   )
-// }
-
-// export default App
--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,19 +1,91 @@
 import {
   Checkbox,
+  Container,
+  Grid,
   FormControlLabel,
-  TextField,
+  Paper,
   Typography,
 } from '@mui/material'
 
-const CityCard = ({ currentWeather }) => {
+import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded'
+import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded'
+
+import { useState } from 'react'
+
+const CityCard = ({
+  currentWeather,
+  setFavouriteLocations,
+  formerFavourite,
+}) => {
+  const [favourited, setFavourited] = useState(formerFavourite)
+  const [tempCelcius, setTempCelcius] = useState(true)
+
+  const onChange = () => {
+    setFavourited((prev) => !prev)
+    const location = currentWeather.location.name.toLowerCase()
+    if (favourited) {
+      setFavouriteLocations((prev) => prev.filter((name) => name !== location))
+    } else {
+      setFavouriteLocations((prev) => {
+        if (prev && prev.includes(location)) return prev
+        const tmpArr = [location, ...prev]
+
+        return tmpArr
+      })
+    }
+  }
+
   return (
-    <div>
-      <Typography variant="h3">
-        {currentWeather.current.temp_c}&deg;C
-      </Typography>
-      <Typography variant="h3">{currentWeather.location.name}</Typography>
-      <Typography variant="h6">{currentWeather.location.country}</Typography>
-    </div>
+    <Grid container direction="column">
+      {/* <Item> */}
+      <Paper
+        elevation={3}
+        sx={{ width: '400px', height: '400px', borderRadius: '5%' }}
+      >
+        {/* Favourite Icon */}
+        <Grid item textAlign={'end'}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={favourited}
+                onChange={onChange}
+                icon={<FavoriteBorderRoundedIcon />}
+                checkedIcon={<FavoriteRoundedIcon />}
+              />
+            }
+          />
+        </Grid>
+        {/* Location */}
+        <Grid item>
+          <Typography variant="h4">{currentWeather.location.name}</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="subtitle1">
+            {currentWeather.location.country}
+          </Typography>
+        </Grid>
+        {/* Weather Icon */}
+        <Grid item>
+          <img
+            src={currentWeather.current.condition.icon}
+            alt="w-icon"
+            loading="lazy"
+          />
+        </Grid>
+        {/* Temperature C/F */}
+        <Grid item>
+          <Typography
+            variant="h4"
+            onClick={() => setTempCelcius((prev) => !prev)}
+          >
+            {tempCelcius
+              ? `${currentWeather.current.temp_c.toFixed()}\u00B0C`
+              : `${currentWeather.current.temp_f.toFixed()}\u00B0F`}
+          </Typography>
+        </Grid>
+      </Paper>
+      {/* </Item> */}
+    </Grid>
   )
 }
 
